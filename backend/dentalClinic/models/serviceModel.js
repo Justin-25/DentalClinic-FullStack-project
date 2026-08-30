3a48bbcd93a68c6306dcad73f438ace6addbddf1
JustinDev <justinmandia25@gmail.com>
2026-08-31 01:47:25 +0800

    Create a Service Schema

---
+++ backend/dentalClinic/models/serviceModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const serviceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A service must have a name...']
+  },
+  slug: String,
+  imageCover: {
+    type: String,
+    default: 'default.jpg'
+  },
+  price: {
+    type: Number,
+    required: [true, 'A service must have a price...']
+  },
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price...'
+    }
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A service must have a duration...']
+  },
+  category: {
+    type: String,
+    enum: ['Preventive & Diagnostic', 'Restorative', 'Surgical & Specialized', 'Cosmetic', 'Emergency & Urgent Care', 'Consultation'],
+    required: [true, 'A service must belong to a category']
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A service must have summary...']
+  },
+  secretService: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    select: false
+  }
+})
