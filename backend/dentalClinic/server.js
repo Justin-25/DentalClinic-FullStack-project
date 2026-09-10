b339c14ca2c75d9ed3173712f250e91dfdf8848c
JustinDev <justinmandia25@gmail.com>
2026-09-11 02:15:16 +0800

    Add README

--- backend/dentalClinic/server.js
+++ backend/dentalClinic/server.js
@@ -19,6 +19,6 @@ mongoose.connect(DB).then(() => {
   console.log(`DB connection successfully!!!`);
 });
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
